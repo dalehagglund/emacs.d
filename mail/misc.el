f726adc6095436967fc8ec87f57a79012e0f99ff
Dale Hagglund <dale.hagglund@gmail.com>
2012-01-03 01:31:03 -0700

    fix `user-mail-address'

--- mail/misc.el
+++ mail/misc.el
@@ -2,8 +2,8 @@
 
 (setq mail-user-agent 'message-user-agent
       mail-host-address "gmail.com"
-      compose-mail-check-user-agent nil
-      user-mail-address	"dale.hagglund@gmail.com.com"
+      compose-mail-check-user-agent-warnings nil
+      user-mail-address	"dale.hagglund@gmail.com"
       rmail-confirm-expunge nil)       ; I don't use rmail, but . . . 
 
 (defvar rdh:bcc-to-self t 
