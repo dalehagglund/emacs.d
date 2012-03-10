e1157493f6863df0f23af0ddc862ff516ae003d2
Dale Hagglund <dale.hagglund@gmail.com>
2012-03-10 11:39:33 -0700

    set up nose.el
     - require nose.el when setting up for python
     - add local keybindings for nose.el in python mode buffers

--- prog/python.el
+++ prog/python.el
@@ -6,9 +6,19 @@
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python2" . python-mode))
 
+(require 'nose)
+
 (defun rdh::python-mode-hook ()
   (define-key py-mode-map [(meta f)] 'py-forward-into-nomenclature)
   (define-key py-mode-map [(meta b)] 'py-backward-into-nomenclature)
-  (define-key py-mode-map [(meta d)] 'py-kill-word-into-nomenclature))
+  (define-key py-mode-map [(meta d)] 'py-kill-word-into-nomenclature)
+  (local-set-key "\C-ca" 'nosetests-all)
+  (local-set-key "\C-cm" 'nosetests-module)
+  (local-set-key "\C-c." 'nosetests-one)
+  (local-set-key "\C-cpa" 'nosetests-pdb-all)
+  (local-set-key "\C-cpm" 'nosetests-pdb-module)
+  (local-set-key "\C-cp." 'nosetests-pdb-one))
+
 
 (add-hook 'python-mode-hook 'rdh::python-mode-hook)
+(setq nose-project-names nil)
