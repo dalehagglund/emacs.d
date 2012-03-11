bb1f37db1da52a35b14b83ff87da29fbe566fb53
Dale Hagglund <dale.hagglund@gmail.com>
2012-03-11 17:29:28 -0600

    add nose.el keybindings with fewer ups/downs on the control key

--- init/prog/python.el
+++ init/prog/python.el
@@ -12,12 +12,21 @@
   (define-key py-mode-map [(meta f)] 'py-forward-into-nomenclature)
   (define-key py-mode-map [(meta b)] 'py-backward-into-nomenclature)
   (define-key py-mode-map [(meta d)] 'py-kill-word-into-nomenclature)
+
   (local-set-key "\C-ca" 'nosetests-all)
   (local-set-key "\C-cm" 'nosetests-module)
   (local-set-key "\C-c." 'nosetests-one)
+
+  (local-set-key "\C-c\C-a" 'nosetests-all)
+  (local-set-key "\C-c\C-m" 'nosetests-module)
+
   (local-set-key "\C-cpa" 'nosetests-pdb-all)
   (local-set-key "\C-cpm" 'nosetests-pdb-module)
-  (local-set-key "\C-cp." 'nosetests-pdb-one))
+  (local-set-key "\C-cp." 'nosetests-pdb-one)
+
+  (local-set-key "\C-c\C-p\C-a" 'nosetests-pdb-all)
+  (local-set-key "\C-c\C-p\C-m" 'nosetests-pdb-module)
+  (local-set-key "\C-c\C-p." 'nosetests-pdb-one))
 
 
 (add-hook 'python-mode-hook 'rdh::python-mode-hook)
